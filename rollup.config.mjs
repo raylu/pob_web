c69a9c052987e8e388ec5290831bbc5ecdb16b8f
raylu <90059+raylu@users.noreply.github.com>
2023-08-22 23:27:09 -0700

    typescript

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,19 @@
 import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import ignore from 'rollup-plugin-ignore';
 
 export default {
-	input: 'src/index.mjs',
+	input: 'src/index.mts',
 	output: {
 		dir: 'www',
 		format: 'es',
 	},
 	plugins: [
+		esbuild({
+			loaders: {'.mts': 'ts'},
+			minify: false,
+		}),
 		nodeResolve(),
 		commonjs(),
 		ignore(['path', 'fs', 'child_process', 'crypto', 'url', 'module']),
---
+++ src/index.mts
@@ -0,0 +1,17 @@
+import {LuaFactory} from 'wasmoon';
+
+const factory = new LuaFactory();
+const lua = await factory.createEngine();
+
+async function runFile(path: string) {
+	const resp = await fetch(path);
+	await lua.doString(await resp.text());
+}
+
+try {
+	// Set a JS function to be a global lua function
+	lua.global.set('sum', (x, y) => x + y);
+	runFile("lua/Launch.lua");
+} finally {
+	lua.global.close();
+}
