3acd7298c9d67dc683a947331fce5bb77aaf4bcc
raylu <90059+raylu@users.noreply.github.com>
2023-08-24 00:51:26 -0700

    load more of PoB lua

---
+++ www/globals.lua
@@ -0,0 +1,34 @@
+function unpack(t, i) -- from https://www.lua.org/pil/5.1.html
+	i = i or 1
+	if t[i] ~= nil then
+		return t[i], unpack(t, i + 1)
+	end
+end
+
+bit = {} -- from https://github.com/lua/lua/blob/master/testes/bitwise.lua
+function bit.bnot (a)
+	return ~a & 0xFFFFFFFF
+end
+function bit.band (x, y, z, ...)
+	if not z then
+		return ((x or -1) & (y or -1)) & 0xFFFFFFFF
+	else
+		local arg = {...}
+		local res = x & y & z
+		for i = 1, #arg do res = res & arg[i] end
+		return res & 0xFFFFFFFF
+	end
+end
+function bit.bor (x, y, z, ...)
+	if not z then
+		return ((x or 0) | (y or 0)) & 0xFFFFFFFF
+	else
+		local arg = {...}
+		local res = x | y | z
+		for i = 1, #arg do res = res | arg[i] end
+		return res & 0xFFFFFFFF
+	end
+end
+function bit.rshift(a, b)
+	return ((a & 0xFFFFFFFF) >> b) & 0xFFFFFFFF
+end
